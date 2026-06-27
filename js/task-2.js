6ef1834d8dc5b4516ba6236316c4e765145c3707
maryna_serdiuk <serdyukmarina876@gmail.com>
2026-06-27 17:18:57 +0200

    updated

--- js/task-2.js
+++ js/task-2.js
@@ -4,9 +4,6 @@ const totalPrice = deliveryFee + price;
     return `Shipping to ${country} will cost ${totalPrice} credits`
 }
 
-getShippingMessage ("Australia", 120, 50);
-getShippingMessage ("Germany", 80, 20);
-getShippingMessage ("Sweden", 100, 20);
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
